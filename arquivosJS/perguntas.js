f195357b42428f8b2314bea9b9b90bb8b8998226
juliana-franca <juliana.franca.santos@escola.pr.gov.br>
2025-07-01 11:44:57 +0000

    OK

---
+++ arquivosJS/perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'MEDICINA',
+    'Qual é a principal função do sistema cardiovascular?',
+    ' O sistema cardiovascular tem como principal função transportar sangue, nutrientes, gases (como oxigênio e dióxido de carbono) e produtos metabólicos pelo corpo. Isso é feito através do coração e dos vasos sanguíneos.'
+)
+
+criaCartao(
+    'MEDICINA',
+    'O que é a hipertensão arterial?',
+    'A hipertensão arterial é uma condição médica caracterizada por níveis elevados e constantes de pressão arterial. Se não tratada, pode aumentar o risco de doenças graves como acidente vascular cerebral (AVC), infarto do miocárdio e insuficiência renal.'
+)
+
+criaCartao(
+    'MEDICINA',
+    'O que são anticorpos?',
+    'Anticorpos são proteínas produzidas pelo sistema imunológico em resposta a substâncias estranhas chamadas antígenos (como vírus, bactérias ou toxinas). Eles ajudam a neutralizar ou destruir essas substâncias, protegendo o corpo de infecções.'
+)
+
+criaCartao(
+    'MEDICINA',
+    'Qual a diferença entre diabetes tipo 1 e tipo 2?',
+    'O diabetes tipo 1 é uma condição autoimune onde o sistema imunológico destrói as células do pâncreas que produzem insulina, enquanto no diabetes tipo 2 o corpo não usa a insulina de forma eficaz ou não produz insulina suficiente.'
+)
+
+criaCartao(
+    'MEDICINA',
+    'O que é uma infecção viral?',
+    'Uma infecção viral ocorre quando um vírus invade e se multiplica nas células do corpo. Exemplos incluem gripe, COVID-19, HIV e hepatite. Ao contrário das infecções bacterianas, os vírus não podem ser tratados com antibióticos.'
+)
+
+criaCartao(
+    ' MEDICINA',
+    'O que é um exame de ressonância magnética (RM)?',
+    'A ressonância magnética (RM) é um exame de imagem que utiliza um campo magnético e ondas de rádio para criar imagens detalhadas das estruturas internas do corpo, como cérebro, músculos, ossos e órgãos. É utilizado para diagnosticar várias condições médicas, como tumores e lesões.'
+)
